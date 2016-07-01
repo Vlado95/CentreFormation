9dcb6853665094fc72a46bc318665cf1a3f91fc6
Jean Vladimir TWAHIRWA <vtwahirwa@scores-decisions.com>
2016-07-01 13:00:26 +0200

    creation du script de la base de donnée

---
+++ src/java/modele/centre-formation-javaee.sql
@@ -0,0 +1,177 @@
+﻿-- Change le délimiteur pour pouvoir écrire des ; dans la
+-- procédure stockée
+DELIMITER §
+
+DROP SCHEMA IF EXISTS centre_formation §
+CREATE SCHEMA IF NOT EXISTS centre_formation DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci §
+USE centre_formation §
+
+CREATE TABLE IF NOT EXISTS promotion (
+  id_promotion INT NOT NULL AUTO_INCREMENT,
+  nom VARCHAR(45) NOT NULL,
+  PRIMARY KEY (id_promotion),
+  UNIQUE INDEX nom_UNIQUE (nom ASC))
+ENGINE = InnoDB§
+
+
+CREATE TABLE IF NOT EXISTS personne (
+  id_personne INT NOT NULL AUTO_INCREMENT,
+  nom VARCHAR(45) NOT NULL,
+  prenom VARCHAR(45) NOT NULL,
+  email VARCHAR(45) NOT NULL,
+  PRIMARY KEY (id_personne),
+  UNIQUE INDEX email_UNIQUE (email ASC))
+ENGINE = InnoDB§
+
+
+CREATE TABLE IF NOT EXISTS membre_promotion (
+  id_promotion INT NOT NULL,
+  id_personne INT NOT NULL,
+  PRIMARY KEY (id_promotion, id_personne),
+  CONSTRAINT fk_membre_promotion_promotion
+    FOREIGN KEY (id_promotion)
+    REFERENCES promotion (id_promotion)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_membre_promotion_personne
+    FOREIGN KEY (id_personne)
+    REFERENCES personne (id_personne)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB§
+
+
+CREATE TABLE IF NOT EXISTS projet (
+  id_projet INT NOT NULL AUTO_INCREMENT,
+  id_promotion INT NOT NULL,
+  id_createur INT NOT NULL,
+  sujet TEXT NOT NULL,
+  titre VARCHAR(100) NOT NULL,
+  date_creation DATETIME NOT NULL,
+  date_limite DATETIME NOT NULL,
+  PRIMARY KEY (id_projet),
+  CONSTRAINT fk_projet_promotion
+    FOREIGN KEY (id_promotion)
+    REFERENCES promotion (id_promotion)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_projet_personne
+    FOREIGN KEY (id_createur)
+    REFERENCES personne (id_personne)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB§
+
+
+CREATE TABLE IF NOT EXISTS equipe (
+  id_equipe INT NOT NULL AUTO_INCREMENT,
+  id_projet INT NOT NULL,
+  id_createur INT NOT NULL,
+  date_creation DATETIME NOT NULL,
+  resume VARCHAR(255) NULL,
+  PRIMARY KEY (id_equipe),
+  CONSTRAINT fk_equipe_projet
+    FOREIGN KEY (id_projet)
+    REFERENCES projet (id_projet)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_equipe_personne
+    FOREIGN KEY (id_createur)
+    REFERENCES personne (id_personne)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB§
+
+
+CREATE TABLE IF NOT EXISTS membre_equipe (
+  id_equipe INT NOT NULL,
+  id_personne INT NOT NULL,
+  PRIMARY KEY (id_equipe, id_personne),
+  CONSTRAINT fk_membre_equipe_equipe
+    FOREIGN KEY (id_equipe)
+    REFERENCES equipe (id_equipe)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_membre_equipe_personne
+    FOREIGN KEY (id_personne)
+    REFERENCES personne (id_personne)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB§
+
+
+DROP PROCEDURE IF EXISTS centre_formation_refresh §
+CREATE DEFINER=root@localhost PROCEDURE centre_formation_refresh()
+BEGIN
+	-- Lever temporairement les contraintes d'intégrité
+	SET FOREIGN_KEY_CHECKS=0;
+	TRUNCATE equipe;
+	TRUNCATE membre_equipe;
+	TRUNCATE membre_promotion;
+	TRUNCATE personne;
+    TRUNCATE projet;
+    TRUNCATE promotion;
+	-- Remettre les contraintes d'integrite
+	SET FOREIGN_KEY_CHECKS=1;
+
+	
+	BEGIN
+	  DECLARE EXIT HANDLER FOR SQLSTATE '23000'
+	  BEGIN
+		SHOW ERRORS;
+        ROLLBACK;
+	  END;
+		START TRANSACTION;
+        
+	INSERT INTO personne(id_personne,nom, prenom, email) VALUES
+		(1, 'Haddock', 'Archibald', 'haddock@moulinsart.be'),
+		(2,'Castafiore', 'Bianca', 'bianca.castafiore@scala.it'),
+		(3, 'Tournesol', 'Tryphon', 'tournesol@moulinsart.be'),
+		(4,'Lampion', 'Séraphin', 'lampion@mondass.fr'),
+		(5,'Krad', 'Imen', 'imkr@yahoo.fr'),
+		(6,'Siby', 'Abdoulaye', 'absiby@yahoo.fr'),
+		(7, 'Buittot', 'Eleanor', 'bui.elea@gamil.com'),
+		(8,'Mickael', 'Angelo', 'M.angelo@tortue.fr'),
+		(9,'Eddy', 'Kenzo', 'edke@yahoo.fr'),
+		(10,'Feyte', 'Floria', 'f.feyte@gamil.com');
+
+	INSERT INTO promotion (id_promotion, nom) VALUES
+		(1, 'Decisionel et developpement JEE'),
+		(2, 'MOA');
+
+	INSERT INTO membre_promotion (id_promotion, id_personne) VALUES
+		(1, 1),
+		(1, 2),
+		(1, 3),
+		(1, 4),
+		(1,5),
+		(1, 6),
+      
+		(2, 7),
+		(2, 8);
+		
+	INSERT INTO projet (id_projet,id_promotion, id_createur,sujet,titre,date_creation, date_limite ) VALUES
+		(1, 1,9,'gestion de terrain','flow managment project','2016-4-12 22:00:00','2016-7-12 22:00:00'),
+		(2, 1,10,'interface contenteiux','scores-decision project','2016-5-15 22:00:00','2016-8-14 22:00:00'),
+		(3, 1,9,'gerer les annonces legales','socores-decisions project','2016-6-13 22:00:00','2016-9-11 22:00:00');
+
+	INSERT INTO equipe (id_equipe, id_projet,id_createur,date_creation) VALUES
+		(1, 1,2,'2016-4-20 22:00:00'),
+        (2, 1,3,'2016-4-23 22:00:00'),
+        (3, 2,1,'2016-5-21 22:00:00'),
+        (4, 1,4,'2016-5-1 22:00:00');
+        
+	INSERT INTO membre_equipe (id_equipe,id_personne) VALUES
+		(1, 2),
+        (1, 5),
+        (2, 3),
+        (2, 1),
+        (3, 1),
+        (3, 4),
+        (4, 4),
+        (4, 6);	
+	  COMMIT;
+	END;
+END§
+
+call centre_formation_refresh()§
