b6eed48961f8c2e314198df902043215df8097fe
Jean Vladimir TWAHIRWA <vtwahirwa@scores-decisions.com>
2016-07-01 15:28:29 +0200

    debut du projet

---
+++ src/java/modele/Projet.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modele;
+
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Vladimir
+ */
+public class Projet {
+    
+    public void insert() throws SQLException{
+        
+    }
+    
+    public static Projet getById(int id){
+    throw new UnsupportedOperationException("pas implemente");
+    }
+    
+}
