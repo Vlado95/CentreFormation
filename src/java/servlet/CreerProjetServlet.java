c91c4ffe7592ce332b7c6c691efa8ac2d41f8f37
Jean Vladimir TWAHIRWA <vtwahirwa@scores-decisions.com>
2016-07-01 17:01:47 +0200

    ajouter le creerProjetServlet pour creer le projet

---
+++ src/java/servlet/CreerProjetServlet.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Vladimir
+ */
+@WebServlet(name = "CreerProjetServlet", urlPatterns = {"/creerProjet"})
+public class CreerProjetServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    
+     String VUE_CrP = "WEB-INF/formProjet.jsp";
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getRequestDispatcher(VUE_CrP).forward(request, response);
+       
+    }
+
+}
