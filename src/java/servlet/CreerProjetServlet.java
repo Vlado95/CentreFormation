90b29d61ecdd241facc2536c4b40794c5572937b
Okito <Okito@PC_OKITO>
2016-07-07 10:51:56 +0200

    Ajoute de la methode post

--- src/java/servlet/CreerProjetServlet.java
+++ src/java/servlet/CreerProjetServlet.java
@@ -7,11 +7,15 @@ package servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import modele.Projet;
 
 /**
  *
@@ -35,6 +39,23 @@ public class CreerProjetServlet extends HttpServlet {
             throws ServletException, IOException {
         request.getRequestDispatcher(VUE_CrP).forward(request, response);
        
+    }
+    
+   
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            String nom = request.getParameter("nom");
+            double prix = Double.parseDouble(request.getParameter("prix"));
+            Projet projet = new Projet(null, id_promotion, id_createur, sujet, titre, date_creation, date_limite );
+            projet.insert();
+            request.setAttribute("message", "Produit ajouté sous le n" + projet.getId());
+        } catch (SQLException ex) {
+            Logger.getLogger(CreerProjetServlet.class.getName()).log(Level.SEVERE, null, ex);
+            request.setAttribute("erreur", ex.getMessage());
+        }
+        request.getRequestDispatcher("WEB-INF/fromProduit.jsp").forward(request, response);
+
     }
 
 }
