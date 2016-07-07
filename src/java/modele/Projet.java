a06159c86a68bb5d0a631b4bdbc89f52c3a5f846
Jean Vladimir TWAHIRWA <vtwahirwa@scores-decisions.com>
2016-07-07 11:16:29 +0200

    test insert

--- src/java/modele/Projet.java
+++ src/java/modele/Projet.java
@@ -5,15 +5,35 @@
  */
 package modele;
 
+//import java.sql.Date;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Date;
 
 /**
  *
  * @author Vladimir
  */
 public class Projet {
-    
+
     private int id;
+    private int id_promotion;
+    private int id_createur;
+    private String titre;
+    private String sujet;
+    private Date date_creation;
+    private Date date_limite;
+
+    public Projet(int id, int id_promotion, int id_createur, String titre, String sujet, Date date_creation, Date date_limite) {
+        this.id = id;
+        this.id_promotion = id_promotion;
+        this.id_createur = id_createur;
+        this.titre = titre;
+        this.sujet = sujet;
+        this.date_creation = date_creation;
+        this.date_limite = date_limite;
+    }
 
     public int getId() {
         return id;
@@ -23,7 +43,80 @@ public class Projet {
         this.id = id;
     }
 
+    public int getId_promotion() {
+        return id_promotion;
+    }
+
+    public void setId_promotion(int id_promotion) {
+        this.id_promotion = id_promotion;
+    }
+
+    public int getId_createur() {
+        return id_createur;
+    }
+
+    public void setId_createur(int id_createur) {
+        this.id_createur = id_createur;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public String getSujet() {
+        return sujet;
+    }
+
+    public void setSujet(String sujet) {
+        this.sujet = sujet;
+    }
+
+    public Date getDate_creation() {
+        return date_creation;
+    }
+
+    public void setDate_creation(Date date_creation) {
+        this.date_creation = date_creation;
+    }
+
+    public Date getDate_limite() {
+        return date_limite;
+    }
+
+    public void setDate_limite(Date date_limite) {
+        this.date_limite = date_limite;
+    }
+
     public void insert() throws SQLException {
+
+        Connection connection = Database.getConnection();
+        // Commencer une transaction
+        connection.setAutoCommit(false);
+
+        // Inserer le projet
+        String sql = "INSERT INTO projet(id_promotion, id_createur,titre,sujet,date_creation,date_limite) VALUES(?,?,?,?,?,?)";
+        PreparedStatement stmt = connection.prepareStatement(sql);
+        stmt.setInt(1, id_promotion);
+        stmt.setInt(2, id_createur);
+        stmt.setString(1, titre);
+        stmt.setString(2, sujet);
+        stmt.setDate(1, (java.sql.Date) date_creation);
+        stmt.setDate(2, (java.sql.Date) date_limite);
+        stmt.executeUpdate();
+        stmt.close();
+        // Recuperer le id
+      //  Statement maxStmt = connection.createStatement();
+      //  ResultSet rs = maxStmt.executeQuery("SELECT MAX(no_produit) AS id FROM produit");
+      //  rs.next();
+      //  id = rs.getInt("id");
+    //    rs.close();
+     //   maxStmt.close();
+        // Valider
+      //  connection.commit();
 
     }
 
--- test/modele/ProjetTest.java
+++ test/modele/ProjetTest.java
@@ -5,6 +5,8 @@
  */
 package modele;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -20,19 +22,20 @@ public class ProjetTest {
     @Test
     public void testInsert() throws Exception {
         System.out.println("insert");
-        Projet instance = new Projet();
+        Projet instance;
+        instance = new Projet(1,2,9,"lol","blalba",new SimpleDateFormat("yyyy-MM-dd").parse("2007-03-06"),new SimpleDateFormat("yyyy-MM-dd").parse("20-03-06"));
         instance.insert();
         assertEquals(4, instance.getId());
         Projet expected = Projet.getById(4);
         assertEquals(expected, instance);
     }
 
-    @Test
-    public void testGetById() {
+  /*  @Test
+    public void testGetById() throws ParseException {
         System.out.println("getById");
-        Projet expected = new Projet();// avec les valeurs du 1
+        Projet expected = new Projet(1,2,9,"lol","blalba",new SimpleDateFormat("yyyy-MM-dd").parse("2007-03-06"),new SimpleDateFormat("yyyy-MM-dd").parse("20-03-06"));// avec les valeurs du 1
         Projet result = Projet.getById(1);
         assertEquals(expected, result);
-    }
+    }*/
     
 }
